077d97e718025fb8bff0c75eed968b2d078acda8
sakuya55688 <jerry281123@gmail.com>
2022-01-27 22:30:22 +0800

    canvas drawing function

---
+++ src/Canvas.js
@@ -0,0 +1,63 @@
+import "./Canvas.css"
+
+const Canvas = () => {
+    
+    window.addEventListener("load", function(){
+        const canvas = document.querySelector("#canvas");
+        const ctx = canvas.getContext('2d');
+        
+        canvas.height = 400;
+        canvas.width = 800;
+        
+        let painting = false;
+        let lineWidth = 1.7;
+        let mouse = {x: 0, y: 0};
+
+        function startPosition(e){
+            painting = true;
+
+            /* Making Dot Possible */
+            ctx.lineTo(e.pageX - this.offsetLeft -1, e.pageY - this.offsetTop -1);
+            ctx.stroke();
+            draw(e);
+        }
+        function finishedPosition(e){
+            painting = false;
+            ctx.beginPath();
+        }
+        function changeCursor(){
+            canvas.style.cursor = "crosshair";
+        }
+
+        function draw(e){
+            if(!painting) return;
+            ctx.lineWidth = lineWidth;
+            ctx.lineCap = "round";
+
+            //ctx.lineTo(e.clientX, e.clientX);
+            ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        }
+
+
+        canvas.addEventListener("pointerdown", startPosition);
+        canvas.addEventListener("pointerup", finishedPosition);
+        canvas.addEventListener("pointermove", draw);
+        canvas.addEventListener("pointerover", changeCursor);
+    });
+    
+    
+    
+    
+
+    return ( 
+        <>
+            <canvas id="canvas"></canvas>
+        </>
+     );
+}
+ 
+export default Canvas;
