03e5ec0e334dd78776fcd732a2368d68ee67486d
sakuya55688 <jerry281123@gmail.com>
2022-01-28 22:13:30 +0800

    make initialization with react hooks and add line width with pressure

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,65 +1,104 @@
 import "./Canvas.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Canvas = () => {
-    
-    window.addEventListener("load", function(){
-        const canvas = document.querySelector("#canvas");
-        const ctx = canvas.getContext('2d');
-        
+
+    const commonColors = ["red","blue","yellow","green","purple"];
+
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [lineWidth, setLineWidth] = useState(1.7);
+    const [lineColor, setLineColor] = useState("black");
+
+    const canvasRef = useRef(null);
+    const ctxRef = useRef(null);
+
+    //initial canvas and context
+    useEffect(() => {
+
+        const canvas = canvasRef.current;
         canvas.height = 400;
         canvas.width = 800;
         
-        let painting = false;
-        let lineWidth = 1.7;
-        let mouse = {x: 0, y: 0};
+        const ctx = canvas.getContext('2d');
+        
+        ctx.lineCap = "round";
+        ctxRef.current = ctx;
 
-        function startPosition(e){
-            painting = true;
-
-            /* Making Dot Possible */
-            ctx.lineTo(e.pageX - this.offsetLeft -1, e.pageY - this.offsetTop -1);
-            ctx.stroke();
-            draw(e);
-        }
-        function finishedPosition(e){
-            painting = false;
-            ctx.beginPath();
-        }
-        function changeCursor(){
-            canvas.style.cursor = "crosshair";
-        }
-
-        function draw(e){
-            if(!painting) return;
-            ctx.lineWidth = lineWidth;
-            ctx.lineCap = "round";
-
-            //ctx.lineTo(e.clientX, e.clientX);
-            ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-        }
+    }, []);
 
 
-        canvas.addEventListener("pointerdown", startPosition);
-        canvas.addEventListener("pointerup", finishedPosition);
-        canvas.addEventListener("pointermove", draw);
-        canvas.addEventListener("pointerover", changeCursor);
-    });
     
+    const startDrawing = ({nativeEvent, pressure}) => {
+        setIsDrawing(true);
+        const {offsetX , offsetY} = nativeEvent;
+        /* Draw the Dot Possible */
+        ctxRef.current.lineWidth = lineWidth * pressure * 4.5;
+        ctxRef.current.strokeStyle = lineColor;
+
+        ctxRef.current.lineTo(offsetX , offsetY);
+        ctxRef.current.stroke();
+        draw({nativeEvent});
+
+        //console.log(pressure * 8)
+        
+    }
+    const finishDrawing = (e) => {
+        setIsDrawing(false);
+        ctxRef.current.beginPath();
+    }
+    function changeCursor(){
+        canvasRef.current.style.cursor = "crosshair";
+    }
+    const draw = ({nativeEvent,pressure}) => {
+        
+        if(!isDrawing) return;
+        
+        const {offsetX, offsetY} = nativeEvent;
+        ctxRef.current.lineWidth = lineWidth * pressure * 5;
+
+        ctxRef.current.lineTo(offsetX, offsetY);
+        ctxRef.current.stroke();
+
+        ctxRef.current.beginPath();
+        ctxRef.current.lineTo(offsetX, offsetY);
+
+        console.log(lineColor);
+    }
+
     
-    
-    
+    function changeColor(value){
+        let newColor = "" + value;
+        setLineColor(newColor);
+        
+    }
+    function changeLineWidth(newWidth){
+        setLineWidth(newWidth);
+    }
 
     return ( 
         <>
-            <canvas id="canvas"></canvas>
+            <canvas id="canvas"
+                onPointerDown={(e) => startDrawing(e)}
+                onPointerUp={finishDrawing}
+                onPointerMove={draw}
+                ref={canvasRef}
+            ></canvas>
 
-            <div className="color-blocks">
-
+            <div className="function-blocks">
+                <button className="undo-btn">Undo</button>
+                <button className="clear-btn">Clear</button>
+                <div className="color-blocks">
+                    {
+                    commonColors.map((color)=>(
+                        <div className="color-block" id={color} key={color}
+                            style={{backgroundColor: "" + color}}
+                            onClick={(e) => changeColor(e.target.id)}
+                        ></div>
+                    ))
+                    }
+                </div>
+                <input type="color" onInput={(e) => changeColor(e.target.value)} className="color-picker" />
+                <input type="range" onInput={(e) => changeLineWidth(e.target.value)} className="line-width" />
             </div>
         </>
      );
